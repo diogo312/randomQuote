860776e668c92998af3725416c628d2fb1d70631
diogo312 <56561874+diogo312@users.noreply.github.com>
2023-05-30 20:12:55 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './'
+  optimizeDeps: {
+    // include: ['linked-dep'],
+  },build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+      // include: [/linked-dep/, /node_modules/],
+    },
+  },
 })
